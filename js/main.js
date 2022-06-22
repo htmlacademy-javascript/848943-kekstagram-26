5bd2d6be5fd61d45c9130296304b86bb100f07b9
Webrissae <l.komarnickaya@yandex.ru>
2022-06-22 22:28:52 +0500

    упрощает код, исправляет магические числа

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно
-
-const getRandomNumber = (min,max) => {
-  if (min < max && min >= 0) {
-    const random = Math.round((Math.random() * (max-min)) + min);
-    return random;
-  }
-  if (min < 0) {
-    return 'Диапазон может быть только положительный';
-  }
-  return 'Первое число должно быть меньше второго';
-};
-
-getRandomNumber(6, 10);
-
-//Функция для проверки максимальной длины строки
-const checkTextLength = (text, maxLength) => text.length <= maxLength;
-checkTextLength('Какой-то текст', 50);
-
-//Функция от Кекса, возвращающая случайное целое число
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -25,22 +5,7 @@ const getRandomPositiveInteger = (a, b) => {
   return Math.floor(result);
 };
 
-//module4-task1
-
 const PHOTOS_COUNT = 25;
-
-//Функция для поиска уникального идентификатора в заданном диапазоне
-const getRandomUnicID = (min, max, arr) => {
-  let unicID = 0;
-  do {
-    unicID = getRandomPositiveInteger(min, max);
-  }
-  while (arr.includes(unicID) && arr.length <= max);
-  arr.push(unicID);
-  return unicID;
-};
-
-//массив - описание фотографий
 const PHOTO_DESCRIPTIONS = [
   'Пляж',
   'В направлении лета',
@@ -69,7 +34,6 @@ const PHOTO_DESCRIPTIONS = [
   'Водные процедуры'
 ];
 
-//массив - тексты для комментариев
 const PHRASES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -79,7 +43,6 @@ const PHRASES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?'
 ];
 
-//Имена авторов
 const AUTHORS = [
   'Мимо проходила',
   'Кедра Выдра',
@@ -89,72 +52,55 @@ const AUTHORS = [
   'КиссМисс'
 ];
 
-//функция создания комментария из случайно выбранных 1-2 фраз
-
-const mixPhrases = () => {
-  const comments = [];
-  const phrasesQty = getRandomPositiveInteger(1, 2);
-  do {
-    const newComment = getRandomPositiveInteger(0, PHRASES.length-1);
-    comments.push(PHRASES[newComment]);
-  }
-  while (comments.length <= phrasesQty);
-  return comments.join('');
+const LikesCount = {
+  min: 15,
+  max: 200
+};
+const PhrasesCount = {
+  min: 1,
+  max: 2
+};
+const CommentsCount = {
+  min: 0,
+  max: 3
+};
+const AvatarCount = {
+  min: 1,
+  max: 6
 };
 
-//массив из созданных комментариев
-const mixedPhrases = Array.from({length: PHOTOS_COUNT}, mixPhrases);
+let PhotoID = 0;
+let CommentID = 0;
 
-//функция выбора случайного комментария
-
-const getRandomComment = () => {
-  const commentsQty = getRandomPositiveInteger(1, 3);
-  const randomCommentIDs = [];
-  const unicCommentIDs = []; //кэширование идентификаторов комментариев
-
-  for (let i = 0; i < commentsQty; i++) {
-    const newCommentID = getRandomUnicID(1, PHOTOS_COUNT, unicCommentIDs);
-    randomCommentIDs.push(newCommentID);
+const createMessage = () => {
+  const phrasesCount = getRandomPositiveInteger(PhrasesCount.min, PhrasesCount.max);
+  const message = [];
+  for (let i = 1; i <= phrasesCount; i++) {
+    message.push(PHRASES[getRandomPositiveInteger(0, PHRASES.length -1)]);
   }
-  return randomCommentIDs;
+  return [...  new Set(message)].join(' ');
 };
 
-//массив идентификаторов списков комментариев
-const commentListIDs = Array.from({length: PHOTOS_COUNT*3}, getRandomComment);
-
-//создание массива из списков текстов комментариев с именами авторов
-
-const createComment = (element) => {
-  const commentsCash = [];
+const createComment = () => {
+  CommentID++;
   return {
-    id: getRandomUnicID(1, PHOTOS_COUNT*3, commentsCash),
-    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
-    message: mixedPhrases[element],
-    name: AUTHORS[getRandomPositiveInteger(0,AUTHORS.length-1)]
+    id: CommentID,
+    avatar: `img/avatar-${getRandomPositiveInteger(AvatarCount.min, AvatarCount.max)}.svg`,
+    message: createMessage(),
+    name: AUTHORS[getRandomPositiveInteger(0, AUTHORS.length - 1)]
   };
 };
 
-const COMMENTS = commentListIDs.map((item) => item.map((element) => createComment(element)));
-
-//кэширование уникальных идентификаторов
-const photoIDsCash = [];
-const commentsIDsCash = [];
-
-//создание объектов-фото
 const createPhoto = () => {
-
-  const randomPhotoIndex = getRandomUnicID(0, PHOTOS_COUNT-1, photoIDsCash);
-  const randomCommentArr = getRandomUnicID(1, COMMENTS.length-1, commentsIDsCash);
-
+  PhotoID++;
   return {
-    id: randomPhotoIndex,
-    url: `photos/${  randomPhotoIndex  }.jpg`,
-    description: PHOTO_DESCRIPTIONS[randomPhotoIndex],
-    likes: getRandomPositiveInteger(15, 200),
-    comments: COMMENTS[randomCommentArr]
+    id: PhotoID,
+    url: `photos/${  getRandomPositiveInteger(1, PHOTO_DESCRIPTIONS.length)  }.jpg`,
+    description: PHOTO_DESCRIPTIONS[getRandomPositiveInteger(0, PHOTO_DESCRIPTIONS.length - 1)],
+    likes: getRandomPositiveInteger(LikesCount.min, LikesCount.max),
+    comments: Array.from({length: getRandomPositiveInteger(CommentsCount.min, CommentsCount.max)}, createComment)
   };
 };
 
-const photoGallery = Array.from({length: PHOTOS_COUNT}, createPhoto);
-// eslint-disable-next-line no-console
-console.log(photoGallery);
+const createPhotoGallery = () => Array.from({length: PHOTOS_COUNT}, createPhoto);
+createPhotoGallery();
