fcd84a16b4d2eafa71ea959b0d94c8fd9e080ab4
Webrissae <l.komarnickaya@yandex.ru>
2022-06-25 12:01:35 +0500

    123

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+import './setup.js';
+import './data.js';
+import './raw-data.js';
+import './util.js';
 import {createPhotoGallery} from './data.js';
 
 createPhotoGallery();
+console.log(createPhotoGallery());
