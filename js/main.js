f466948f017d95a49deacb09cc6c455cf23bcc9c
Webrissae[C <l.komarnickaya@yandex.ru>
2022-07-06 20:03:39 +0500

    add big picture preview

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-import {picturesContainer} from './thumbnails.js';
+import {drawThumbnails} from './draw-thumbnails.js';
+import {picturesContainer} from './draw-thumbnails.js';
+import {addThumbnailClickHandler} from './draw-big-picture.js';
 
-const getPicturesContainer = () => picturesContainer;
-getPicturesContainer();
+const picturesList = picturesContainer.children;
+
+drawThumbnails();
+
+for (let i = 2; i < picturesList.length - 2; i++) {
+  addThumbnailClickHandler(picturesList[i]);
+}
